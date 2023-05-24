690f7d2a9a0a16d31fb677f118b4b486001c313a
Hellmann <PPA859@ad.ku.de>
2023-05-24 09:04:34 +0200

    Zwischencommit

--- server_altInput.r
+++ server_altInput.r
@@ -1,90 +1,55 @@
 # paramNames <- c("z", "sz", "a", "v", "sv", "tau",
 #                 "delta_t", "input$n_sim", "max_rt")
-require(dplyr)
-require(tidyr)
-require(ggplot2)
-library(ggpubr)
-
-input <- list(c = 0.05, sd=1, sv=1, w=0.5, n_sim= 100, 
-              manipulation1 = c(0, 1, 2, 3),manipulation2 = c(1, 3),
-              Einfluss_man1_d = 2, Einfluss_man1_v=0.5, 
-              Einfluss_man2_d=1, Einfluss_man2_v=1,
-              name_man1 = "Orientierung", name_man2="Kontrast"
-              )
-              # man1 = "d, v", funcs1 = "x, abs(x)", steps1 = "1,2,3,4",
-              # man1 = "w", funcs1 = "x", steps1 = "0.2, 0.8")
+input <- list(z=0.5, sz=0, sv=0.1, v=0.5, a=1.5, delta_t=0.05, max_rt=10, n_sim=100,
+              tau=1, s=1,
+              man1 = "d, v", funcs1 = "x, abs(x)", steps1 = "1,2,3,4",
+              man1 = "w", funcs1 = "x", steps1 = "0.2, 0.8")
               
-
-list2env(input, envir = environment())
-conditions <- expand.grid(man1 = manipulation1, man2=manipulation2)
-
-conditions$d  <- conditions$man1*Einfluss_man1_d + 
-  conditions$man2*Einfluss_man2_d
-conditions$v  <- conditions$man1*Einfluss_man1_v + 
-  conditions$man2*Einfluss_man2_v
-conditions$sd <- input$sd
-conditions$sv <- input$sv
-conditions$w <- input$w
-
-
-stim <- rep(c(-1, 1), each=input$n_sim)
-
-
-sim <- conditions %>% group_by(man1, man2) %>%
-  reframe(X = rnorm(2*input$n_sim, mean=c(rep(c(d/2,-d/2), each=n_sim)), input$sd),
-          V = rnorm(2*input$n_sim, v, input$sv),
-          stim = rep(c(1, -1), each=n_sim)) %>% 
-  mutate(resp = (-1)^(X < input$c),
-         correct = as.numeric(resp==stim),
-         conf = (1-input$w)*abs(X-input$c) + input$w *V)
-ggplot(sim, aes(x=X, linetype=as.factor(man2), color=as.factor(man1),
-                group=interaction(stim, man1, man2)))+
-  scale_color_discrete(name=input$name_man1)+
-  scale_linetype_discrete(name=input$name_man2)+
-  geom_density()
-p_d_man1 <- ggplot(sim, aes(x=X, linetype=as.factor(stim), color=as.factor(man1)))+
-  scale_color_discrete(name=input$name_man1)+
-  scale_linetype_discrete(name="Stimuluskategorie")+
-  geom_density()
-p_d_man2 <- ggplot(sim, aes(x=X, linetype=as.factor(stim), color=as.factor(man2)))+
-  scale_color_discrete(name=input$name_man2)+
-  scale_linetype_discrete(name="Stimuluskategorie")+
-  geom_density()
-ggarrange(p_d_man1, p_d_man2, nrow=2)
-p_v_man1 <- ggplot(sim, aes(x=V, linetype=as.factor(stim), color=as.factor(man1)))+
-  scale_color_discrete(name=input$name_man1)+
-  scale_linetype_discrete(name="Stimuluskategorie")+
-  geom_density()
-p_v_man2 <- ggplot(sim, aes(x=V, linetype=as.factor(stim), color=as.factor(man2)))+
-  scale_color_discrete(name=input$name_man2)+
-  scale_linetype_discrete(name="Stimuluskategorie")+
-  geom_density()
-ggarrange(p_v_man1, p_v_man2, nrow=2)
-ggarrange(p_d_man1, p_d_man2,p_v_man1, p_v_man2, layou)
-gridExtra::grid.arrange(p_d_man1, p_v_man1, p_d_man2, p_v_man2, nrow=2)
-# D = eval(parse(text=input$discriminability))
-sim
-
-
+#input <- list(c=0.2, sv=1, v=1.5, d=1, sd=1, w=0.5, n_sim=500)
+# 
+server <- function(input, output, session) {
+  
+  simulate_observations <- reactive({
+    list2env(input, envir = environment())
+    require(dplyr)
+    require(tidyr)
+    set.seed(ceiling(input$d*input$n_sim*input$v*as.numeric(input$sd)*input$sv*input$w)*
+               ceiling(abs(as.numeric(Sys.time())-round(as.numeric(Sys.time()), -2))+if_else(is.null(input$recalc), 1, 2)))   
+    stim <- rep(c(-1, 1), each=input$n_sim)
+    X <- rnorm(2*input$n_sim, input$d*stim, input$sd)
+    V <- rnorm(2*input$n_sim, input$v, input$sv)
+    resp <- (-1)^(X < input$c)
+    correct <- resp*stim
+    conf <- (1-input$w)*abs(X-input$c)+input$w*V
+    sim <- data.frame(X, V, resp, correct, conf, stim=stim)
+    # D = eval(parse(text=input$discriminability))
+    sim
+  })
+  
   # gen_plot <- reactive({
   #   
   # })
   
   output$pathsplot <- renderPlot({
     input$recalc
-    # pars1 <- as.character(trimws(unlist(strsplit(input$man1,","))))
-    # func1 <- as.character(trimws(unlist(strsplit(input$funcs1,","))))
-    # funcs1 <- list()
-    # for (i in 1:length(func1)) {
-    #   eval(parse(text = paste('funcs1[[',i,']]', #pars1[i], 
-    #                           ' <- function(x) { return(' , func1[i] , ')}', sep='')))
-    # }
-    # for (i in 1:length(func1)) {
-    #   eval(parse(text = paste('funcs1[[',i,']]', #pars1[i], 
-    #                           ' <- function(x) { return(' , func1[i] , ')}', sep='')))
-    # }
-    # funcs1
-
+    pars1 <- as.character(trimws(unlist(strsplit(input$man1,","))))
+    func1 <- as.character(trimws(unlist(strsplit(input$funcs1,","))))
+    funcs1 <- list()
+    for (i in 1:length(func1)) {
+      eval(parse(text = paste('funcs1[[',i,']]', #pars1[i], 
+                              ' <- function(x) { return(' , func1[i] , ')}', sep='')))
+    }
+    for (i in 1:length(func1)) {
+      eval(parse(text = paste('funcs1[[',i,']]', #pars1[i], 
+                              ' <- function(x) { return(' , func1[i] , ')}', sep='')))
+    }
+    funcs1
+    
+    
+    pars1 <- as.character(trimws(unlist(strsplit(input$man1,","))))
+    pars1 <- as.character(trimws(unlist(strsplit(input$man1,","))))
+    pars1 <- as.character(trimws(unlist(strsplit(input$man1,","))))
+    
     sim <- simulate_observations()
     {  
       library(ggplot2)
